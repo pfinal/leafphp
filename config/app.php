7427bec71a7e40325514867a232977e87e0d3609
ethan <190196539@qq.com>
2017-01-06 11:00:51 +0800

    route.cache

--- config/app.php
+++ config/app.php
@@ -3,6 +3,9 @@
 // 应用基准目录
 $app['path'] = dirname(__DIR__);
 
+//是否开启路由缓存(缓存暂不支持闭包路由)
+$app['route.cache'] = false;
+
 $app->register(new \Leaf\Provider\LogServiceProvider());
 $app->register(new \Leaf\Provider\DatabaseServiceProvider());
 $app->register(new \Leaf\Provider\TwigServiceProvider());
