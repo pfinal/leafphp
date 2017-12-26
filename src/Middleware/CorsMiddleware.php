89da20089a68673cc4c52ee8d6f7a2c076cdf022
ethan <nicesoft333@qq.com>
2017-12-26 16:11:05 +0800

    cors

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -6,11 +6,26 @@ class CorsMiddleware
 {
     public function handle(\Leaf\Request $request, \Closure $next)
     {
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS
+        $headers = array(
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'X-PINGOTHER, Content-Type',
+            'Access-Control-Max-Age' => 86400,
+        );
+
+        if ($request->isMethod('options')) {
+            $response = new \Symfony\Component\HttpFoundation\Response();
+            $response->headers->add($headers);
+            return $response;
+        }
+
         $response = $next($request);
         if (!$response instanceof \Symfony\Component\HttpFoundation\Response) {
             $response = new \Symfony\Component\HttpFoundation\Response($response);
         }
-        $response->headers->add(array('Access-Control-Allow-Origin' => '*'));
+        $response->headers->add($headers);
         return $response;
     }
 }
