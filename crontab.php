38f6b416e35b39084b57bdb5c3a4db9f1ea5d2b7
Ethan <nicesoft333@qq.com>
2018-11-22 14:19:27 +0800

    crontab

---
+++ crontab.php
@@ -0,0 +1,26 @@
+<?php
+
+//php crontab.php start
+//php crontab.php start -d
+//php crontab.php start status
+
+//composer require workerman/workerman
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Workerman\Worker;
+use \Workerman\Lib\Timer;
+
+Worker::$logFile = __DIR__ . '/app/runtime/crontab.log';
+Worker::$pidFile = __DIR__ . '/app/runtime/crontab.pid';
+
+$task = new Worker();
+
+$task->onWorkerStart = function ($task) {
+
+    //每60秒执行一次
+    Timer::add(60, function () {
+        exec('php ' . __DIR__ . '/console crontab', $out);
+    });
+};
+
+Worker::runAll();
