6f390a5f71ba1669464becd969bf104cc330216c
Ethan <nicesoft333@qq.com>
2019-12-08 11:16:04 +0800

    env

--- config/app.php
+++ config/app.php
@@ -11,6 +11,10 @@ $app->register(new \Leaf\Provider\CaptchaProvider());
 $app->register(new \Leaf\Provider\CacheProvider());
 $app->register(new \Leaf\Provider\QueueProvider());
 
+if (file_exists(__DIR__ . '/../.env')) {
+    \Dotenv\Dotenv::create(__DIR__ . '/../')->load();
+}
+
 $app->registerBundle(new \AppBundle\AppBundle());
 
 //如果开启路由缓存，则不支持使用闭包路由
@@ -33,12 +37,13 @@ $app['twig.app'] = $app->extend('twig.app', function ($twigApp, $app) {
 
 //数据库连接配置
 $app['db.config'] = array(
-    'host' => 'localhost',
-    'database' => 'test',
-    'username' => 'root',
-    'password' => '',
+    'host' => getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost',
+    'database' => getenv('DB_DATABASE') ? getenv('DB_DATABASE') : 'test',
+    'username' => getenv('DB_USERNAME') ? getenv('DB_USERNAME') : 'root',
+    'password' => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '',
+    'tablePrefix' => getenv('DB_PREFIX') ? getenv('DB_PREFIX') : '',
+    'port' => getenv('DB_PORT') ? getenv('DB_PORT') : 3306,
     'charset' => 'utf8mb4',
-    'tablePrefix' => '',
 );
 
 if (file_exists(__DIR__ . '/app-local.php')) {
